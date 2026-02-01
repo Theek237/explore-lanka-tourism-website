ca6473a8ab2ef9f5d7b010b0c827133b59654f72
Theekshana Alahakoon <theek237@gmail.com>
2026-02-01 13:14:41 +0530

    check pipeline

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -74,7 +74,7 @@ export default function App() {
             }
           />
 
-          {/* Fallback */}
+          {/* Fallback ttesr */}
           <Route
             path="*"
             element={
